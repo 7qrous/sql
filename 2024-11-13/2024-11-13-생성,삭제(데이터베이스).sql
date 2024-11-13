1295640ce9b3a95ef4bc569513a4ef73460198ea
7qrous <louis.develog@gmail.com>
2024-11-13 15:25:30 +0900

    update

--- 2024-11-13/2024-11-13-생성,삭제(데이터베이스).sql
+++ 2024-11-13/2024-11-13-생성,삭제(데이터베이스).sql
@@ -144,9 +144,79 @@ create table testtable6 (
     foreign key (user_type) references usercate(user_type) 
 );
 
+-- sql 문으로 테이블 수정
+-- 사용법
+-- alter table 테이블명 add | drop | modify column 컬럼명 데이터타입;
+-- add : 컬럼 추가시 사용, 컬럼의 데이터 타입 필수
+-- drop : 컬럼 삭제시 사용
+-- modify : 컬럼 수정 시 사용, 컬럼의 데이터 타입 필수
+
+select * from testtable1;
+
+alter table testtable1 add column etc varchar(3000);
+select * from testtable1;
+
+-- 제약조건도 추가할수있음
+alter table testtable1 add column update_at datetime not null;
+select * from testtable1;
+
+alter table testtable1 drop column update_at;
+select * from testtable1;
+
+alter table testtable1 modify column user_address varchar(500) not null;
+select * from testtable1;
+
+-- alter 명령을 사용하여 기본키 (PK) 지정
+alter table testtable1 add primary key (num);
+alter table testtable1 modify column num int auto_increment;
+
+-- alter 명령을 사용하여 기본키 설정 해제
+-- 먼저 auto increment 제거
+alter table testtable1 modify column num int;
+alter table testtable1 drop primary key;
+
+-- alter 명령을 사용하여 참조키(FK) 설정
+-- 기존 테이블에 참조키를 추가하려면 참조키로 사용할 컬럼을 먼저 추가해야한다.
+alter table testtable1 add column user_type varchar(5);
+alter table testtable1 add foreign key (user_type) references usercate(user_type);
+
+-- alter 명령을 사용하여 참조키 설정 제거
+-- 참조키 설정을 먼저 삭제하고, 참조키로 사용한 컬럼 삭제
+ALTER TABLE testtable1 DROP FOREIGN KEY testtable1_ibfk_1;
+ALTER TABLE testtable1 DROP COLUMN user_type;
+
+select * from testtable1;
+-- CONSTRAINT
+-- 기본키, 참조키와 같은 같은 제약조건을 설정할 떄 해당 제약 조건의 이름을 지정하는 명령
+-- create table, alter table 에서 사용 가능
 
 
+create table testtable7 (
+	num int not null auto_increment,
+    user_id varchar(45) not null,
+    user_pw varchar(45) not null,
+    user_name varchar(45) not null,
+    user_email varchar(45) not null,
+    create_at datetime,
+    constraint pk_num primary key(num)
+);
 
+drop table testtable7;
+
+create table testtable7 (
+	num int not null,
+    user_id varchar(45) not null,
+    user_pw varchar(45) not null,
+    user_name varchar(45) not null,
+    user_email varchar(45) not null,
+    user_type varchar(5) not null,
+    create_at datetime,
+    constraint pk_num primary key(num),
+    constraint fk_usertype foreign key ( user_type) references usercate( user_type )
+);
+
+alter table testtable7 drop foreign key fk_usertype;
+alter table testtable7 drop primary key ;
 
 
 
